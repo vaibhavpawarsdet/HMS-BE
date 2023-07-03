7cc6bb4194ddc32c74f9f51525201219bcafe784
vaibhavpawarsdet <vaibhavpawarsdet@gmail.com>
2023-07-03 18:22:53 +0530

    profiller controller updated

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,13 +1,14 @@
-import Profile from "../models/profile.js";
+import Profile from "../models/profile.js"
 import User from "../models/user.js";
 
 //get profile information
 export const getProfileDetails = async (req, res) => {
     try {
-        const userId = req.user._id;
+        console.log(req.user);
+        const userId = req.user.userId;
 
         //find user by id and populate the username and email fields
-        const user = await User.findOne( userId, "username email");
+        const user = await User.findById(userId);
 
         if (!user) {
             return res.status(404).json({ message: "User not found" });
@@ -38,64 +39,38 @@ export const getProfileDetails = async (req, res) => {
 
 export const profileUpdate = async (req, res) => {
     try {
-        const userId = req.user._id;
-        const {age, sex, phone, address } = req.body;
+        const userId = req.user.userId;
+        console.log(userId);
+        console.log(req.body);
+        const { age, sex, phone, address } = req.body;
 
         //Find the profile associated with the user
         const profile = await Profile.findOne({ user: userId });
-        if (!profile) {
-            return res.status(404).json({ message: "Profile not found" });
-        };
 
+        if (profile) {
+            let updatedProfile = {
+                age: profile.age,
+                sex: profile.sex,
+                profilePhoto: profile.profilePhoto,
+                phone: profile.phone,
+                address: profile.address,
+            };
+            //Handle profile photo upload
+            if (req.file) {
+                updatedProfile.profilePhoto = req.file.filename;
+            }
+            await profile.updateOne(updatedProfile, { _id: profile._id })
+            return res.status(200).json(profile);
+        };
+        const newProfile = new Profile({
+            age, sex, phone, address, user: userId,
+        });
         //update the profile fields
-        profile.age = age;
-        profile.sex = sex;
-        profile.phone = phone;
-        profile.address = address;
-
-        //Handle profile photo upload
-        if (req.file) {
-            profile.profilePhoto = req.file.filename;
-        }
-
         //Save the updated profile
-        profile = await profile.save();
-
-        //combine the updated profile data
-        const updatedProfile = {
-            age: profile.age,
-            sex: profile.sex,
-            profilePhoto: profile.profilePhoto,
-            phone: profile.phone,
-            address: profile.address,
-        };
-
-        return res.status(200).json(updatedProfile);
+        await newProfile.save();
+        return res.status(200).json(newProfile);
     } catch (error) {
         console.error(error);
         return res.status(500).json({ message: "Internal server error" });
     };
 };
-
-//For uploading profile photo
-// export const uploadProfilePhoto = async (req, res) => {
-//     try {
-//         const userId = req.user._id;
-
-//         //find profile associate with the user
-//         let profile = await Profile.findOne({ user: userId });
-
-//         if (!profile) {
-//             return res.status(404).json({ message: "Profile not found" });
-//         }
-
-//         //Save the uploaded profile photo filename to the profile
-//         profile.profilePhoto = req.file.filename;
-//         await profile.save();
-
-//         return res.status(200).json({ message: "Profile photo uploaded successfully" });
-//     } catch (error) {
-//         console.error(error);
-//         return res.status(500).json({ message: "Internal server error" });
-//     };
-// };
