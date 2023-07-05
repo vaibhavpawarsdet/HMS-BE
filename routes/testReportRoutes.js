a2703aae5c5c58729d0d92ccf66672d4bb1d08ce
vaibhavpawarsdet <vaibhavpawarsdet@gmail.com>
2023-07-05 17:50:55 +0530

    user controller update with role and testing report, profile routes updated with auth middleware

--- routes/testReportRoutes.js
+++ routes/testReportRoutes.js
@@ -2,11 +2,12 @@ import express from "express";
 import { generateTestReport } from "../controllers/testReportController.js";
 import { getTestReportByPatientName } from "../controllers/testReportController.js";
 import { getAllTestReport } from "../controllers/testReportController.js";
+import checkAuth from "../middleware/checkAuth.js";
 
 const router = express.Router();
 
-router.route('/test-report').post( generateTestReport );
-router.route('/test-report/:patientName').get( getTestReportByPatientName );
-router.route('/test-report').get( getAllTestReport );
+router.route('/test-report').post( checkAuth("Admin"), generateTestReport );
+router.route('/test-report/:patientName').get( checkAuth("Admin"), getTestReportByPatientName );
+router.route('/test-report').get(checkAuth("Admin"), getAllTestReport );
 
 export default router;
