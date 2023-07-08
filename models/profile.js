d576cddbf07c11f4d042152288b6f400f9bd7551
vaibhavpawarsdet <vaibhavpawarsdet@gmail.com>
2023-07-08 09:49:35 +0530

    profile models remove default values

--- models/profile.js
+++ models/profile.js
@@ -11,37 +11,29 @@ const profileSchema = new mongoose.Schema({
     },
     patientId: {
         type: String,
-        default: "Empty",
         unique: true,
-        required: true,
+
     },
     profilePhoto: {
-        type: String,
+        data: Buffer,
+        contentType: String,
     },
     age: {
         type: Number,
-        default: 0,
-        required: [true, "Please provide age"],
         maxlength: [2, "Age should be under 2 characters"]
     },
     gender: {
         type: String,
-        default: "Empty",
-        enum: ["Male", "Female", "Other", "Empty"],
-        required: true,
+        enum: ["Male", "Female", "Other"],
     },
     
     phone: {
         type: String,
-        default: "0",
-        required: true,
         maxlength: [10, "Phone should be under 10 characters"],
         validate: [validator.isNumeric, "Please enter number into correct format"],
     },
     address: {
         type: String,
-        default: "Empty",
-        required: true,
         maxlength: [100, "Address should be under 100 characters"],
     },
 },
