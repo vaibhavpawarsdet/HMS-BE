d3dfc717c1b66749dbc8a48498b46bbfceafe6c4
vaibhavpawarsdet <vaibhavpawarsdet@gmail.com>
2023-07-08 19:43:23 +0530

    update and get profile updated

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -4,7 +4,7 @@ import User from "../models/user.js";
 //get profile information
 export const getProfileDetails = async (req, res) => {
     try {
-       // console.log(req.user); 
+        // console.log(req.user); 
         const userId = req.user.userId;
 
         //find user by id and populate the username and email fields
@@ -18,18 +18,23 @@ export const getProfileDetails = async (req, res) => {
         const profile = await Profile.findOne({ user: userId });
 
         if (!profile) {
-            return res.status(404).json({ message: "Profile not found" });
+            const profile = new Profile({
+                user: userId,
+                username: user.username,
+                email: user.email,
+                patientId: "",
+                age: "",
+                gender: "Male",
+                phone: "",
+                address: "",
+            });
+            await profile.save();
+            //return res.status(404).json({ message: "Profile not found" });
         };
 
         const profileData = {
             username: user.username,
             email: user.email,
-            patientId: profile.patientId,
-            profilePhoto: profile.profilePhoto,
-            age: profile.age,
-            gender: profile.gender,
-            phone: profile.phone,
-            address: profile.address,
         };
         return res.status(200).json(profileData);
     } catch (error) {
@@ -41,36 +46,30 @@ export const getProfileDetails = async (req, res) => {
 export const profileUpdate = async (req, res) => {
     try {
         const userId = req.user.userId;
-       // console.log(userId);
-       // console.log(req.body);
-        const { patientId, profilePhoto, age, gender, phone, address } = req.body;
+        //console.log(userId);
+        //console.log(req.body);
+
+        const { patientId, age, gender, phone, address } = req.body;
 
         //Find the profile associated with the user
         const profile = await Profile.findOne({ user: userId });
-
-        if (profile) {
-            let updatedProfile = {
-                patientId: profile.patientId,
-                profilePhoto: profile.profilePhoto,
-                age: profile.age,
-                gender: profile.gender,
-                phone: profile.phone,
-                address: profile.address,
-            };
-            //Handle profile photo upload
-            if (req.file) {
-                updatedProfile.profilePhoto = req.file.filename;
-            }
-            Object.assign(updatedProfile, req.body);
-            await profile.updateOne(updatedProfile, { _id: profile._id })
-            return res.status(200).json(profile);
+        //console.log(profile);
+        if (!profile) {
+            return res.status(404).json({ message: "Profile not found" });
+        }
+        let updatedProfile = {
+            patientId: patientId || profile.patientId,
+            age: age || profile.age,
+            gender: gender || profile.gender,
+            phone: phone || profile.phone,
+            address: address || profile.address,
         };
-        const newProfile = new Profile({
-            user: userId, patientId, profilePhoto, age, gender, phone, address,
-        });
-        //Save the updated profile
-        await newProfile.save();
-        return res.status(200).json(newProfile);
+        //Handle profile photo upload
+        if (req.file) {
+            updatedProfile.profilePhoto = req.file.filename;
+        }
+        await profile.updateOne(updatedProfile, { _id: profile._id })
+        res.status(200).json(updatedProfile);
     } catch (error) {
         console.error(error);
         return res.status(500).json({ message: "Internal server error" });
