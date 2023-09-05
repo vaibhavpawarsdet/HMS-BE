daec9942a8ae0a45eec13de585918b35034ba5fb
Vaibhav Pawar <128985843+vaibhavpawarsdet@users.noreply.github.com>
2023-09-05 13:09:04 +0530

    Update billingRoutes.js

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { createBills, getBill } from "../controllers/billingController.js";
+import { createBills, getBill, addListsPrices, getListPrices } from "../controllers/billingController.js";
 import checkAuth from "../middleware/checkAuth.js";
 
 const router = express.Router();
 
 router.route('/addbill').post(checkAuth("Admin"), createBills);
 router.route('/getbill').get(checkAuth("Admin"), getBill);
+router.route('/additems').post(addListsPrices);
+router.route('/getlist').get(checkAuth("Admin"), getListPrices);
 
-export default router;
+export default router; 
