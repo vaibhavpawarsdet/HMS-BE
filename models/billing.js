15ba68be317978d36c5b20286fe9d66432af7773
Vaibhav Pawar <128985843+vaibhavpawarsdet@users.noreply.github.com>
2023-09-07 13:07:16 +0530

    Update billing.js

--- models/billing.js
+++ models/billing.js
@@ -10,7 +10,7 @@ const billingModel = new mongoose.Schema({
         required: true,
     },
     price: {
-        type: Number,
+        type: String,
         required: true,
     },
     totalPrice: {
