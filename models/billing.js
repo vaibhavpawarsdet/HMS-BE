a792611bc12f29480a329c2774a3d9b88fd4671b
Vaibhav Pawar <128985843+vaibhavpawarsdet@users.noreply.github.com>
2023-09-07 14:53:47 +0530

    Update billing.js

--- models/billing.js
+++ models/billing.js
@@ -3,19 +3,15 @@ import mongoose from "mongoose";
 const billingModel = new mongoose.Schema({
     item: {
         type: String,
-        required: true,
     },
     quantity: {
         type: Number,
-        required: true,
     },
     price: {
         type: String,
-        required: true,
     },
     totalPrice: {
         type: Number,
-        required: true,
     },
 },
     { timestamps: true }
